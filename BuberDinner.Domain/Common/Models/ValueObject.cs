8e1a9d31c7a3a50f1a2909a9e0deae5ce0b67961
Furkan Bilal <bilalyigit594@gmail.com>
2023-09-22 17:22:31 +0300

    Part 12 ValueObject, Entity and AggregateRoot

---
+++ BuberDinner.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BuberDinner.Domain.Common.Models
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEqualityComponents();
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+            ValueObject valueObject = (ValueObject)obj;
+
+            return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals((object?)other);
+        }
+    }
+
+    public class Price : ValueObject
+    {
+        public decimal Amount { get; private set; }
+        public string Currency { get; private set; }
+
+        public Price(string currency, decimal amount)
+        {
+            Currency = currency;
+            Amount = amount;
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Amount;
+            yield return Currency;
+        }
+    }
+}
